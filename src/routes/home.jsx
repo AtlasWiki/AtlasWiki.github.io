d19bf9be645c691d49e02108918a7a488485c257
AtlasWiki <nathanwong3421@gmail.com>
2024-08-10 01:30:45 -0700

    adjust transition effects

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -80,8 +80,8 @@ export function Home(){
                         {/* <h2 className="font-mono text-md text-gray-600">{'<'}Cybersecurity Enthusiast{'>'}</h2> */}
                     </div>
                     <div className="flex gap-5">
-                        <a href="mailto:nathanwong3421@gmail.com" className="border-2 border-black px-10 py-2 text-lg text-gray-600 font-semibold hover:bg-gray-500/20">Contact</a>
-                        <button className="bg-black px-10 py-2 text-lg text-gray-400 font-semibold hover:opacity-80" onClick={() => document.location = "https://www.linkedin.com/in/nathan-w-76ba78202/"}>Linkedin</button>
+                        <a href="mailto:nathanwong3421@gmail.com" className="border-2 border-black px-10 py-2 text-lg text-gray-600 font-semibold transition-transform duration-1000 hover:translate-y-2 hover:bg-gray-500/20">Contact</a>
+                        <button className="bg-black px-10 py-2 text-lg text-gray-400 font-semibold hover:opacity-80 transition-transform duration-1000 hover:translate-y-2" onClick={() => document.location = "https://www.linkedin.com/in/nathan-w-76ba78202/"}>Linkedin</button>
                     </div>
                     <a className="" href="https://github.com/AtlasWiki/">
                         <svg className="transition-all duration-1000 hover:size-52" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="black" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/></svg>
